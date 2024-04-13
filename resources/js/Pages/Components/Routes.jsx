2d679a9eeffb5d0a8286a9d9b7c87b3ab03757a3
g610g <gio.gonzales@carsu.edu.ph>
2024-04-14 02:29:46 +0800

    named routes with query string

---
+++ resources/js/Pages/Components/Routes.jsx
@@ -0,0 +1,11 @@
+const Routes = ({ users, create_url }) => {
+  console.log(users);
+  console.log(create_url);
+  return (
+    <div>
+      Hello world
+    </div>
+  );
+};
+
+export default Routes;
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Guest\AuthController as AppAuthController;
 use App\Models\Admin\Admin;
+use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\URL;
 use Inertia\Inertia;
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -27,17 +30,30 @@ Route::group(['prefix' => 'admin'], function () {
 });
 
 // This will be the landing page
-Route::get('/', function () {
-    return Inertia::render('Components/Core/Landing');
+Route::get('/', function (Request $request) {
+    return Inertia::render('Components/Core/Landing', ['user' => $request->query('user') ]);
 })->name('home');
 Route::get('/register', function () {
     return Inertia::render('Components/Core/Register');
-});
-Route::get('/test', function () {
-    return Inertia::render('Testing');
-});
-Route::post('/test', function () {
-    return Inertia::render('Testing', [
-        'data' => 'test'
+})->name('users.create');
+Route::get('/routes', function () {
+    $users = User::factory()->count(5);
+    return Inertia::render('Components/Routes', [
+        'users' => User::all()->map(function ($user) {
+            return [
+                'id' => $user->id,
+                'first_name' => $user->first_name,
+                'last_name' => $user->last_name,
+                'email' => $user->email,
+                'phone_number' => $user->phone_number,
+                'role_id' => $user->role_id,
+                'city' => $user->city,
+                'province' => $user->province,
+                'country' =>  $user->country,
+                'barangay' => $user->barangay,
+                'edit_url' => URL::route('home', ['user' => 'Gio'])
+            ];
+        }),
+        'create_url' => URL::route('users.create'),
     ]);
 });
