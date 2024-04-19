72674bc02c0189a0c4c53a0b88f31a68f07ccb0e
g610g <gio.gonzales@carsu.edu.ph>
2024-04-20 02:16:51 +0800

    shop frontend

--- resources/js/Pages/Components/Core/Shop.jsx
+++ resources/js/Pages/Components/Core/Shop.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import CustomerSideBarLayout from "../Layouts/CustomerSideBarLayout";
 import CarouselMe from "./CarouselMe";
+import SearchIcon from "../../../assets/search.svg?react";
 function Shop() {
     return (
-        <main className=" overflow-auto  max-h-screen h-screen items-center">
+        <main className=" max-h-screen h-screen items-center">
             <CarouselMe />
-            <div className="h-[1500px] w-full">Hello</div>
+            <div className=" mt-[6rem] w-full text-red-700">
+                <div className="w-full flex justify-between ">
+                    <div className="w-[30%] flex gap-3">
+                        <div className="px-4  py-1 rounded-[.9rem] bg-[#19273A] w-[60%] flex items-center">
+                            <p className="text-white font-league text-[1.4rem] font-semibold text-center w-full">
+                                See all products
+                            </p>
+                        </div>
+                        <div className="bg-[#19273A] flex items-center  rounded-[.5rem] p-4">
+                            <SearchIcon />
+                        </div>
+                    </div>
+                    <div className="w-[40%] flex justify-end ">pagination</div>
+                </div>
+            </div>
         </main>
     );
 }
